ad08ed82d574994e8a176fa60be77fb1db78002f
adam2021 <adam.patel101@outlook.com>
2020-10-28 17:14:45 +0000

    boris bikes part 11 - bike docking works

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
-    return Bike.new
+  return Bike.new
   end
+
+  def dock_bike(bike)
+    @bike = bike
+    return @bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,16 @@ require_relative "../lib/docking_station.rb"
 
 describe DockingStation do
 
+  describe "dock_bike" do
+    it { is_expected.to respond_to(:dock_bike).with(1).argument }
+    it { is_expected.to respond_to(:bike)}
+    it { is_expected.to_not eq (nil)}
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock_bike(bike)).to eq bike
+    end
+
+  end
+
 end
